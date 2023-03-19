044dc32fea54ff054a7fda08b32552c3da394fde
Henry Nicholson <dr.ex10011@gmail.com>
2023-03-19 16:58:39 +1300

    It now works...

--- newscript.js
+++ newscript.js
@@ -3,32 +3,8 @@ const ctx = canvas.getContext('2d');
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
-let leftColor = "rgb(255, 0, 0)";
-let rightColor = "rgb(0, 0, 255)";
-
-const hexToRGB = hex => {
-    let alpha = false,
-      h = hex.slice(hex.startsWith('#') ? 1 : 0);
-    if (h.length === 3) h = [...h].map(x => x + x).join('');
-    else if (h.length === 8) alpha = true;
-    h = parseInt(h, 16);
-    return (
-      'rgb' +
-      (alpha ? 'a' : '') +
-      '(' +
-      (h >>> (alpha ? 24 : 16)) +
-      ', ' +
-      ((h & (alpha ? 0x00ff0000 : 0x00ff00)) >>> (alpha ? 16 : 8)) +
-      ', ' +
-      ((h & (alpha ? 0x0000ff00 : 0x0000ff)) >>> (alpha ? 8 : 0)) +
-      (alpha ? `, ${h & 0x000000ff}` : '') +
-      ')'
-    );
-  };
-
-let params = new URLSearchParams(location.search);
-leftColor = hexToRGB(params.get('left-color'));
-rightColor = hexToRGB(params.get('right-color'));
+const leftColor = "rgb(255, 0, 0)";
+const rightColor = "rgb(0, 0, 255)";
 
 
 function interpolateColors(leftColor, rightColor, x) {
