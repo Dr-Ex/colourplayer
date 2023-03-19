02c8f8cc3cf91feb6de390f49f81502311673802
Henry Nicholson <dr.ex10011@gmail.com>
2023-03-19 22:06:37 +1300

    Blackness...

--- colour.js
+++ colour.js
@@ -3,6 +3,9 @@ const ctx = canvas.getContext('2d');
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
+ctx.fillStyle = 'black';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
 let leftColor = "rgb(255, 0, 0)";
 let rightColor = "rgb(0, 0, 255)";
 
